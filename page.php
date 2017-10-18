83cba8d6ef27f166fc43e39f2007c7cde5b53e60
Sahana Umashankar <su83@afsconnect2.njit.edu>
2017-10-17 21:04:14 -0400

    added page.php file

---
+++ page.php
@@ -0,0 +1,25 @@
+<?php
+// Base page extended by other pages of the application
+// TODO: Need to change the url and upload_dir if needed
+abstract class page {
+  protected $html;
+  protected $UPLOAD_DIR = 'uploads/';
+  protected $URL = 'https://web.njit.edu/~su83/Project_Upload/';
+  public function __construct()
+    {
+      $this->html .= '<html>';
+      $this->html .= '<body>';
+    }
+    public function __destruct()
+    {
+      $this->html .= '</body></html>';
+      print_r($this->html);
+    }
+    public function get() {
+      print_r('Generic Get Request, no mapping available');
+    }
+    public function post() {
+      print_r($_POST);
+    }
+}
+?>
